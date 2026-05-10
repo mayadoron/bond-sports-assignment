67afe57cd2d0754ea038904d2f50e4720d2bf1ca
mayadoron <maya.doron5@gmail.com>
2026-05-10 09:51:07 +0300

    refactor: remove unused ResponseTransformInterceptor from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
-import { ResponseTransformInterceptor } from './common/interceptors/response-transform.interceptor';
 import { configureApp, configureSwagger } from './bootstrap';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   configureApp(app);
-  app.useGlobalInterceptors(new ResponseTransformInterceptor());
   configureSwagger(app);
 
   const configService = app.get(ConfigService);
